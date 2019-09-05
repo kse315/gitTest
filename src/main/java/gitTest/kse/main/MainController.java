da223661c728a28f92376c8c5c9c07234790d59e
pcrcfdisk <pcrcfdisk@naver.com>
2019-09-05 21:32:47 +0900

    git테스트 파일 첫번째

---
+++ src/main/java/gitTest/kse/main/MainController.java
@@ -0,0 +1,28 @@
+ 
+package gitTest.kse.main;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Class Name : MainController.java
+ * @Description : Main Controller Class
+ * @Modification Information
+ * @
+ * @  수정일      수정자              수정내용
+ * @ ---------   ---------   -------------------------------
+ * @ 2019.09.05           최초생성 
+ * @ git Test 첫번째
+ *
+ * @author kse
+ * @since 2019. 09.05
+ * @version 1.0
+ * @see
+ *
+ *  
+ */
+
+@Controller
+public class MainController {
+
+	 
+}
